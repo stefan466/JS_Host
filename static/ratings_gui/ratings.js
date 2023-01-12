f4638cd6357ab0a195a91b432a929e52346ae717
Stefan Maksimović <smaksimovic3519rn@raf.rs>
2023-01-12 16:23:49 +0100

    dodat ratings gui

---
+++ static/ratings_gui/ratings.js
@@ -0,0 +1,23 @@
+const cookies = document.cookie.split('=');
+const token = cookies[cookies.length - 1];
+
+function getAllRatinRgs() {
+
+}
+
+function getRatingById() {
+
+}
+
+function deleteRating() {
+
+}
+
+function initPostRating() {
+
+}
+
+
+function initUpdateRating() {
+    
+}
